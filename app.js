3d2fec37c2ac0e566f40fb2198a1db740f882688
cristhian jesus quintana tomayro <cristhian jesus quintana tomayro>
2016-08-24 16:07:21 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*   */
+$("#menu").click(function (){
+	var element = $(this).siblings("nav");
+
+	if($(element).hasClass("mostrar")){
+		$(element).removeClass("mostrar");
+	} else {
+		$(element).addClass("mostrar")
+	}
+});
